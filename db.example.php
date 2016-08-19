7904b03bd43a748d6ed6dbf6da093e8e0f3ab070
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-19 18:13:28 +0800

    [m] db example

---
+++ db.example.php
@@ -0,0 +1,10 @@
+<?php
+// Rename this file to db.php
+// Edit following
+$db=new stdClass();
+$db->host = 'host';
+$db->dbname = 'dbname';
+$db->table = 'table';
+$db->username = 'username';
+$db->password = 'password';
+?>
